2912b717116a216735ccad18149af6e3920aad0a
Janghyunsung2 <tlswlfk@gmail.com>
2026-02-03 18:13:33 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    trustHostHeader: true, // Cloudflare HTTPS 프록시용
-  },
 };
 
 export default withNextIntl(nextConfig);
